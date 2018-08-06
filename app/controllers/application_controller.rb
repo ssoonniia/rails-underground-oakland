9c95b8419e55869de9fa2834e0b54e708b28ce1c
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-08-06 12:54:52 -0400

    sign up login logout and routes added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,7 @@
 class ApplicationController < ActionController::Base
 
   def home
-
-  end 
-
-  def current_user
-    User.find_by_id(session[:user_id])
   end
 
-  def logged_in?
-    !!session[:user_id]
-  end
 
-  def authenticated?
-    if current_user && current_user.authenticate(params[:user][:password])
-    end
-  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def current_user
+    User.find_by_id(session[:user_id])
+  end
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def authenticated?
+    if current_user && current_user.authenticate(params[:user][:password])
+    end
+  end
 end
