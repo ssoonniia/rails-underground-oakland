506e7d2b3519a26ce974506fc64d3a9441aa1491
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-08-06 14:32:42 -0400

    added logout and show to user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,4 +14,9 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    reset_session
+    redirect_to root_path 
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find_by_id(session[:user_id])
+  end
+
   private
 
 
