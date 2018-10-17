7a40dcee8372e6c432205a3ace25140b68d77fe6
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-10-16 21:40:18 -0700

    create a partial layout called messages which holds flash messages. Refractored application and home layout
    moved flash messages to new file and rendering them in needed view.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
 private
 
   def current_user
-    User.find_by_id(session[:user_id])
+    @user = User.find_by_id(session[:user_id])
   end
 
   def set_user
