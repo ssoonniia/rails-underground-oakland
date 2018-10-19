48a4240aa470151fe5f3041a2917a2bd550081fb
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-10-19 16:13:21 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,25 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#home'
-
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
-
   get ':id/delete'=> 'events#destroy'
-
   get 'auth/:provider' => redirect("/auth/google_oauth2")
   get 'auth/:provider/callback' => 'sessions#create_with_google'
   get 'auth/failure' => redirect('/')
-
-
-    resources :users , except: [:destory]
-    resources :events
+  resources :users , except: [:destory]
+  resources :events
 
   resources :events do
-      resources :rsvps, only: [:index]
+   resources :rsvps, only: [:index]
   end
-
 
   resources :user do
-    resources :events do
-      resources :rsvps, only: [:new, :create]
-    end
+   resources :events do
+    resources :rsvps, only: [:new, :create]
+   end
   end
-
 
   resources :rsvps, only: [:destroy]
 end
