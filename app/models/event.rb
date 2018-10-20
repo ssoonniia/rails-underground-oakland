1d9974414ad698435eda612f03c345ce988f4a22
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-10-20 08:45:45 -0700

    added cheapest to filter cost by string value
    other version of cheapest still exists it is grayed out

--- app/models/event.rb
+++ app/models/event.rb
@@ -11,7 +11,8 @@ class Event < ApplicationRecord
   scope :todays_events,  -> {where('date == ?', Date.today) }
   scope :past_events,  -> {where('date < ?',  Date.today)}
   scope :future_events,  -> {where('date >= ?', Date.today)}
-  scope :cheapest, -> {where('cost == ?','Free')}
+  scope :cheapest, -> {(Event.order(cost: :asc).limit(1))}
+  # scope :cheapest, -> {where('cost == ?', "Free")}
 
   def current_rsvp
     self.rsvps.collect do |rsvp|
