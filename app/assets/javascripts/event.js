0bb0efb28fc4876b9f3ddc1795d4af3465850d06
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-10-23 15:57:56 -0700

    add Event constructor

---
+++ app/assets/javascripts/event.js
@@ -0,0 +1,10 @@
+function Event(attributes){
+  this.name = attributes.name;
+  this.id = attributes.id;
+  this.date = attributes.date;
+  this.time = attributes.time;
+  this.location = attributes.location;
+  this.cost = attributes.cost;
+  this.description = attributes.cost;
+  this.user_id = attributes.user_id
+}
