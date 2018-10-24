ab2c032e1b085009f02dad96b4823d7f37d9fde4
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-10-24 15:20:04 -0700

    change success function to append results to proper div
    tested and working

--- app/assets/javascripts/rsvps.js
+++ app/assets/javascripts/rsvps.js
@@ -2,20 +2,25 @@ function Rsvp(attributes){
     this.guest = attributes.guests,
     this.user = attributes.user.username,
     this.event = attributes.event.name
+    this.event_id = attributes.event.id
     // this.event = new Event.(attributes.event)
 }
 
-Rsvp.success = function(json){
-  var rsvps = data.map(function(item){
-    return new Rsvp(item)
-  });
+function success(json){
+   json.forEach(function(item){
+    var rsvp = new Rsvp(item)
+   var results = rsvp.renderLi(rsvp)
+ return $("div#add_guests_"+String(rsvp.event_id)).append(results)
+});
+
+
 //   tested and returned
 // (2) [Rsvp, Rsvp]
 // 0: Rsvp {guest: 1, user: "jack", event: "First Fridays"}
 // 1: Rsvp {guest: 1, user: "Sonia Duarte Nicas", event: "First Fridays"}
 // length: 2
-  var rsvpsLi = rsvps.renderLi();
-  $('.guests').append(rsvpsLi);
+  // var rsvpsLi = rsvps.renderLi();
+;
 }
 
 Rsvp.error = function(response){
@@ -33,17 +38,15 @@ Rsvp.prototype.renderLi = function(){
 $(document).ready(function(){
   Rsvp.source = $("#guests-template").html();
   Rsvp.template = Handlebars.compile(Rsvp.source)
-  debugger ;
+
   $("a.show_rsvps").on('click', function(e){
-    debugger;
+    e.preventDefault()
     $.ajax({
       url: this.href,
       method: "GET",
-      success: function(data){
-       // create js object rsvp
-       debugger;
+      success: function(response){
+        success(response)
       }
     })
-    e.preventDefault()
   })
 })
