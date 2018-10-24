e87768fa4d0098c46d5b189b86dd20b3e9cab415
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-10-24 16:58:20 -0700

    add users.js file in assets
    add users assets file and prototype for user

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,7 @@
+function Users(attributes){
+  this.id = attributes.id,
+  this.username = attributes.username,
+  this.email = attributes.email,
+  this.events = attributes.events
+  this.rsvps = attributes.rsvps
+}
