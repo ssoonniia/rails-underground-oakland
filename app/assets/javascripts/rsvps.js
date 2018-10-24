e4164ec475e82ddacb8c6a755b86f3a5ed055162
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-10-24 10:27:15 -0700

    add rsvp.success function
    function takes the json response, which is an array of objects, and calls rsvp new on each one.
    
    after will render template and append to guest div in show view. These functions are not yet built.

--- app/assets/javascripts/rsvps.js
+++ app/assets/javascripts/rsvps.js
@@ -1,5 +1,14 @@
 function Rsvp(attributes){
-  this.guest = attributes.guests,
-  this.user = attributes.user.username,
-  this.event = attributes.event.name
+    this.guest = attributes.guests,
+    this.user = attributes.user.username,
+    this.event = attributes.event.name
+}
+
+Rsvp.success = function(json){
+  json.forEach(function(item){
+    var rsvp = new Rsvp(item)
+  });
+
+  var rsvpLi = rsvp.renderLi()
+  $('.guests').append(rsvpLi)
 }
