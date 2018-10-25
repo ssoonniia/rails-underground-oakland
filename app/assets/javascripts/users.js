437c2f4e3720dc34207d6a3a4cf3b20d2a3931e6
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-10-25 12:33:17 -0700

    deleted code in users.js -- moved it to events.js
    Event.js should handle request since rendering event info not user.

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,7 +1,1 @@
-function Users(attributes){
-  this.id = attributes.id,
-  this.username = attributes.username,
-  this.email = attributes.email,
-  this.events = attributes.events
-  this.rsvps = attributes.rsvps
-}
+
