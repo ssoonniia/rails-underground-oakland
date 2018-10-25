0fd470c4932a3528f37eb2d1a816fc488ba8d65a
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-10-25 13:02:07 -0700

    updated the success function to clear div before appending new event

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -8,5 +8,36 @@ function Event(attributes){
   this.description = attributes.cost;
   this.user_id = attributes.user_id
   this.rsvps = attributes.rsvps
-  this.attending_user = attributes.attending_user
+  this.attendingUser = attributes.attending_user
 }
+
+function successEvent(json){
+  var event = new Event(json)
+  var eventTemp = event.renderLi()
+  $('div#show_event').html("")
+  $('div#show_event').html(eventTemp)
+}
+
+
+Event.prototype.renderLi = function(){
+  return Event.template(this)
+}
+
+
+$(document).ready(function(){
+ $('a.next').on('click', function(e){
+   e.preventDefault();
+
+   Event.templateSource = $('#event-template').html()
+   Event.template = Handlebars.compile(Event.templateSource);
+
+   $.ajax({
+     url: this.href,
+     method: "GET",
+     dataType: "json",
+     success: function(response){
+       successEvent(response)
+     }
+   })
+  })
+})
