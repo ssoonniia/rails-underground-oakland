de0cbae88a046a7201e0a21d5dfeb2f9d858e2af
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-09 19:28:58 -0800

    Add user constructor

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,1 +1,9 @@
+function User(attributes){
+  this.username = attributes.name
+  this.id = attributes.id
+  this.email = attributes.email
+  this.events = attributes.events
+  this.rsvps = attributes.rsvps
+  this.events = attributes.events
 
+}
