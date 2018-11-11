3b491571e4291fa13770ef1bb7a06824e303eb6a
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-11 12:56:17 -0800

    Add rsvps to user attributes

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :email, :attending_events, :events
+  attributes :id, :username, :email, :attending_events, :events, :rsvps
   has_many :events
   has_many :rsvps
   has_many :attending_events, :through => :rsvps, :source => :event
