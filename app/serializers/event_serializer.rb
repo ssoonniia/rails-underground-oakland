633760a75010e845eeb9935de59bf3dfb9ee7e5e
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-16 15:04:32 -0800

    Delete serialier has many through rsvp
    not needed - received through model

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,8 +1,9 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :name, :date, :time, :location, :cost, :description, :display_date, :display_time, :attending_users, :user 
+  attributes :id, :name, :date, :time, :location, :cost, :description, :display_date, :display_time, :attending_users, :user
   belongs_to :user
   has_many :rsvps
-  has_many :attending_users, through: :rsvps, :source => :user
+  has_many :attending_users
+  # , through: :rsvps, :source => :user
 
   def display_date
     self.object.date.strftime('%B %d, %Y')
